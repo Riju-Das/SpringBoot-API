625546dd2fbc0e50449b0bd30d7866b05d20f9de
Riju Das <riju.das1910@gmail.com>
2026-01-28 01:43:29 +0530

    very simple crud controller

--- src/main/java/com/example/content_calender/controller/ContentController.java
+++ src/main/java/com/example/content_calender/controller/ContentController.java
@@ -2,10 +2,16 @@ package com.example.content_calender.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.util.*;
 import com.example.content_calender.model.Content;
 import com.example.content_calender.repository.ContentCollectionRepository;
-import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 @RestController
@@ -22,5 +28,32 @@ public class ContentController {
     public List<Content> findAll(){
         return repository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public Content getMethodName(@PathVariable Integer id) {
+        return repository.findById(id).orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND , "Content Not Found"));
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public void create(@RequestBody Content content){
+        repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@RequestBody Content content, @PathVariable Integer id){
+        if(!repository.ExistsById(id)){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Content not found");
+        }
+        repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id){
+        repository.delete(id);
+    }
+    
     
 }
--- src/main/java/com/example/content_calender/repository/ContentCollectionRepository.java
+++ src/main/java/com/example/content_calender/repository/ContentCollectionRepository.java
@@ -2,22 +2,58 @@ package com.example.content_calender.repository;
 
 import org.springframework.stereotype.Repository;
 import com.example.content_calender.model.Content;
+import com.example.content_calender.model.Type;
+import com.example.content_calender.model.Status;
 
+import jakarta.annotation.PostConstruct;
+
+import java.time.LocalDateTime;
 import java.util.*;
 
 @Repository
 public class ContentCollectionRepository {
-    private final List<Content> content = new ArrayList<>();
+    private final List<Content> contentList = new ArrayList<>();
 
     public ContentCollectionRepository(){
 
     }
 
     public List<Content> findAll(){
-        return content;
+        return contentList;
     }
 
     public Optional<Content> findById(Integer id){
-        return content.stream().filter(c->c.id().equals(id)).findFirst();
+        return contentList.stream().filter(c->c.id().equals(id)).findFirst();
     }
+
+    public void save(Content content){
+        contentList.removeIf(c->c.id().equals(content.id()));
+        contentList.add(content);
+    }
+
+    public boolean ExistsById(Integer id){
+        return contentList.stream().anyMatch(c->c.id().equals(id));
+    }
+
+    public void delete(Integer id){
+        contentList.removeIf(c->c.id().equals(id));
+    }
+
+    @PostConstruct
+    public void init(){
+        Content c = new Content(
+            1,
+            "My first blog Post",
+            "My first blog post",
+            Status.IDEA,
+            Type.ARTICLE,
+            LocalDateTime.now(),
+            null
+        );
+
+        contentList.add(c);
+    }
+
+
+
 }
